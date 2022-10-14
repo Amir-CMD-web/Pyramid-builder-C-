e1fb7f29a1d6a1d215bd5bdd4d1a7b90c80d54d9
Amir <amir.useini08@gmail.com>
2022-10-14 20:01:08 +0200

    Initial commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿class PyramidPattern
+{
+    static void Main(string[] args)
+    {
+        int spc, len = 1, count;
+        Console.WriteLine("Enter row count : ");
+        count = Convert.ToInt32(Console.ReadLine());
+        spc = count - 1;                                                     
+
+        for (int i = 1; i <= count; i++)
+        {
+            for (int r = 1; r <= spc; r++)
+            {
+                Console.Write(" ");
+            }
+            for (int s = 1; s <= len; s++)
+            {
+                Console.Write("*");
+            }
+            spc--;
+            len = len + 2;
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
